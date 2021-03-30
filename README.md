3238b47ed4577a399674c45554a99e24a126bd0f
AndrejApa <81564427+AndrejApa@users.noreply.github.com>
2021-03-30 20:55:49 +0300

    modified readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # study
+
+My first git repo
--- lessons.go
+++ lessons.go
@@ -1,1 +1,20 @@
-package study
+package main
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+	m := map[int]string{2: "a", 0: "b", 1: "c"}
+
+	values := make([]int, 0, len(m))
+	for v:= range m {
+		values = append(values, v)
+	}
+	sort.Ints(values)
+
+	for _, v := range values {
+		fmt.Println(v, m[v])
+	}
+}
